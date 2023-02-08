12b784982bb26e61d654cfd5eac3e8ec7a8310b9
PetroHrynyk <grynyk82@gmail.com>
2023-02-09 00:27:41 +0200

    done

---
+++ src/components/OurFriends/OurFriendsItem/ContentOfPopover/ContentOfPopover.jsx
@@ -0,0 +1,21 @@
+import { nanoid } from 'nanoid';
+
+import styles from './ContentOfPopover.module.css';
+
+export const ContentOfPopover = ({ workDays }) => {
+	const Item = workDays.map(({ isOpen, from, to, weekday }) => (
+		<li key={nanoid()} className={styles.content__item}>
+			<p className={styles.content__day}> {weekday}</p>
+
+			{isOpen ? (
+				<p className={styles.content__time}>
+					{from} - {to}
+				</p>
+			) : (
+				<p className={styles.content__time}>------------</p>
+			)}
+		</li>
+	));
+
+	return <ul className={styles.content}>{Item}</ul>;
+};
--- src/components/OurFriends/OurFriendsList/OurFriendsList.jsx
+++ src/components/OurFriends/OurFriendsList/OurFriendsList.jsx
@@ -1,9 +1,11 @@
+
 import { OurFriendsItem } from '../OurFriendsItem/OurFriendsItem';
 
+import style from './OurFriendsList.module.css';
 
 export const OurFriendsList = ({ data }) => {
 	return (
-		<ul >
+		<ul className={style.list}>
 			{data.map(({ _id, ...info }) => {
 				return <OurFriendsItem key={_id} data={info} />;
 			})}
