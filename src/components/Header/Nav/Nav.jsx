eee3a1a0ae7f5a51e45c488e5438c73422c4ae91
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2023-02-09 18:21:55 +0200

    update Nav

--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -1,12 +1,11 @@
-import {NavLinkStyled} from "./Nav.styled"
+import { NavLinkStyled } from './Nav.styled';
 
 export const Nav = () => {
   return (
     <>
       <NavLinkStyled to="/news">News</NavLinkStyled>
-      <NavLinkStyled to="/notices">Find pet</NavLinkStyled>
+      <NavLinkStyled to="/notices/:categoryName">Find pet</NavLinkStyled>
       <NavLinkStyled to="/friends">Our friends</NavLinkStyled>
     </>
   );
 };
-
