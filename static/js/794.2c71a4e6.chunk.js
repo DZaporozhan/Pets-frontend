3ab4e44b6197264065e0b6e84957e560879402ea
DZaporozhan <98217616+DZaporozhan@users.noreply.github.com>
2023-02-11 11:50:17 +0000

    Deploying to gh-pages from @ DZaporozhan/Pets-frontend@a51138c560d5324c61cf1815c8d5ae9883368daa 🚀

--- static/js/794.2c71a4e6.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{1794:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("h1",{children:"Not Found page"})}}}]);
-//# sourceMappingURL=794.2c71a4e6.chunk.js.map
