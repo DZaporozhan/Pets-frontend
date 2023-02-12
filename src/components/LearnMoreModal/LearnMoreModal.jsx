da028bf41042fd35789c9e3d1a99d2eaf23e2f5e
Dmytro <DSerSmart@gmail.com>
2023-02-13 00:30:57 +0200

    rev1 Запуш Ден

--- src/components/LearnMoreModal/LearnMoreModal.jsx
+++ src/components/LearnMoreModal/LearnMoreModal.jsx
@@ -1,5 +1,30 @@
-import { Title } from './LearnMoreModal.styled';
+import { Title, NoticePhoto } from './LearnMoreModal.styled';
+
+const defaultPhoto =
+  'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png';
 
 export const LearnMoreModal = ({ noticeData }) => {
-  return <Title>Hello World</Title>;
+  console.log(noticeData);
+  return (
+    <div>
+      <NoticePhoto
+        src={
+          (noticeData.imageURL &&
+            `https://res.cloudinary.com/dqupdbup3/image/upload/${imageURL?.toLowerCase()}`) ||
+          defaultPhoto
+        }
+        alt={noticeData.title}
+      />
+      <Title> {noticeData.title} </Title>
+      <li>{noticeData.category}</li>
+      <li>Name: {noticeData.name}</li>
+      <li>birthday: {noticeData.birthday}</li>
+      <li>Breed: {noticeData.breed}</li>
+      <li>Prace: {noticeData.price}</li>
+      <li>The sex: {noticeData.sex}</li>
+      <li>Email: {noticeData.email}</li>
+      <li>Phone: {noticeData.phone}</li>
+      <li>Comments: {noticeData.comments}</li>
+    </div>
+  );
 };
--- src/components/LearnMoreModal/LearnMoreModal.styled.js
+++ src/components/LearnMoreModal/LearnMoreModal.styled.js
@@ -3,3 +3,8 @@ import styled from 'styled-components';
 export const Title = styled.div`
   text-align: center;
 `;
+export const NoticePhoto = styled.img`
+  object-fit: cover;
+  height: 288px;
+  width: 100%;
+`;
