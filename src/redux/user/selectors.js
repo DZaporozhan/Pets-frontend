423b4dd892479b43ccd01c0cd690818135b54080
Krystyna Sharhorodska <rinjara@gmail.com>
2023-02-12 15:53:31 +0200

    user/pet redux fix

--- src/redux/user/selectors.js
+++ src/redux/user/selectors.js
@@ -1,9 +1,3 @@
-// export const getAuthError = ({ auth }) => auth.error;
-// export const isAuth = ({ auth }) => auth.isLoggedIn;
-// export const getUser = ({ auth }) => auth.user;
-// export const getToken = ({ auth }) => auth.token;
-// export const getIsLoading = ({ auth }) => auth.isAuthLoading;
-
 export const getUserInfo = store => store.user.userInfo;
 export const getIsLoading = store => store.user.isLoading;
 export const getError = store => store.user.error;
--- src/redux/user/slice.js
+++ src/redux/user/slice.js
@@ -3,9 +3,6 @@ import { getUserData } from './operations';
 
 const initialState = {
   userInfo: {},
-  //   token: null,
-  //   isLoggedIn: false,
-  //   isRefreshing: false,
   isLoading: false,
   error: null,
 };
@@ -27,43 +24,10 @@ const userSlice = createSlice({
     [getUserData.pending]: handlePending,
     [getUserData.fulfilled](state, action) {
       state.userInfo = action.payload;
-      //   state.token = null;
-      //   state.isLoggedIn = true;
       state.isLoading = false;
     },
     [getUserData.rejected]: handleRejected,
   },
-
-  //     // LOGIN
-  //     [login.pending]: handlePending,
-  //     [login.fulfilled](state, action) {
-  //       state.user = action.payload.user;
-  //       state.token = action.payload.token;
-  //       state.isLoggedIn = true;
-  //       state.isAuthLoading = false;
-  //     },
-  //     [login.rejected]: handleRejected,
-
-  //     // LOGOUT
-  //     [logout.pending]: handlePending,
-  //     [logout.fulfilled](state) {
-  //       state.user = {
-  //         email: null,
-  //       };
-  //       state.token = null;
-  //       state.isLoggedIn = false;
-  //       state.isAuthLoading = false;
-  //     },
-  //     [logout.rejected]: handleRejected,
-
-  //     [current.pending]: handlePending,
-  //   },
-  //   [current.fulfilled]: (store, { payload }) => {
-  //     store.loading = false;
-  //     store.user = payload;
-  //     store.isLogin = true;
-  //   },
-  //   [current.rejected]: handleRejected,
 });
 
 export const userReducer = userSlice.reducer;
