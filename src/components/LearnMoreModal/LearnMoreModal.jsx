95e176393421f0573c83d2d0e0a4143c77f92b8e
Dmytro <DSerSmart@gmail.com>
2023-02-13 14:49:00 +0200

    rev3

--- src/components/LearnMoreModal/LearnMoreModal.jsx
+++ src/components/LearnMoreModal/LearnMoreModal.jsx
@@ -1,13 +1,30 @@
-import { Container, Title, ImageThumb } from './LearnMoreModal.styled';
+import {
+  Container,
+  Title,
+  ImageThumb,
+  Tumb,
+  ContactBtn,
+} from './LearnMoreModal.styled';
+
+const defaultPhoto =
+  'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png';
 
 export const LearnMoreModal = ({ noticeData }) => {
   console.log(noticeData);
+
   return (
     <Container>
-      <ImageThumb src={noticeData.imageURL} />
-      src={noticeData.imageURL}
-      category={noticeData.category}
       <Title> {noticeData.title} </Title>
+      <Tumb> {noticeData.category}</Tumb>
+      <ImageThumb
+        src={
+          (noticeData.imageURL &&
+            `https://res.cloudinary.com/dqupdbup3/image/upload/${noticeData.imageURL?.toLowerCase()}`) ||
+          defaultPhoto
+        }
+        alt={noticeData.title}
+      />
+
       <li>{noticeData.category}</li>
       <li>Name: {noticeData.name}</li>
       <li>birthday: {noticeData.birthday}</li>
@@ -17,6 +34,10 @@ export const LearnMoreModal = ({ noticeData }) => {
       <li>Email: {noticeData.email}</li>
       <li>Phone: {noticeData.phone}</li>
       <li>Comments: {noticeData.comments}</li>
+      <ContactBtn primary onClick={window.open`tel:${noticeData.phone}`}>
+        Contact
+      </ContactBtn>
     </Container>
   );
 };
+// window.open`tel:${noticeData.phone
