637b03a53669151ca4102bca6df4433e0cb70c16
aleksandr105 <aleksandrsherbina105@gmail.com>
2023-02-15 01:49:00 +0100

    adds color accent  for active button paginate

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,5 +1,23 @@
 import { PaginationStyled } from './Pagination.styled';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  status: {
+    danger: '#e53e3e',
+  },
+  palette: {
+    primary: {
+      main: '#f59256',
+      darker: '#053e85',
+    },
+    neutral: {
+      main: '#64748B',
+      contrastText: '#fff',
+    },
+  },
+});
+
 export const PaginationComponent = ({ paginateData }) => {
   const { setPage, totalPage } = paginateData;
 
@@ -8,13 +26,16 @@ export const PaginationComponent = ({ paginateData }) => {
   };
 
   return (
-    <PaginationStyled
-      count={totalPage}
-      onChange={(_, num) => {
-        setPage(num);
-        scrollToTop();
-      }}
-      size="large"
-    />
+    <ThemeProvider theme={theme}>
+      <PaginationStyled
+        count={totalPage}
+        onChange={(_, num) => {
+          setPage(num);
+          scrollToTop();
+        }}
+        size="large"
+        color="primary"
+      />
+    </ThemeProvider>
   );
 };
