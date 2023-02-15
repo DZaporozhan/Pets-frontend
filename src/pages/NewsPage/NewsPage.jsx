5d5904669b342522a72069d6eaf8383edef6391f
PetroHrynyk <grynyk82@gmail.com>
2023-02-15 20:17:22 +0200

    Update NewsPage.jsx

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -5,7 +5,8 @@ import { NewsList } from "components/News/NewsList/NewsList";
 import {NewsSearch} from "components/News/NewsSearch/NewsSearch"
 import NotFoundPage from "pages/NotFoundPage/NotFoundPage";
 import { Loader } from "components/Loader/Loader";
-
+import { toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 const NewsPage = () => {
   const [news, setNews] = useState([])
   const [isNewsLoading, setIsNewsLoading] = useState(true)
@@ -13,7 +14,12 @@ const NewsPage = () => {
   const [query, setQuery] = useState('');
   const handleSubmit = e => {
     e.preventDefault();
-    // console.log(query);
+    // console.log(query);-----------------------------------
+    if (query.trim() === '') {
+      setQuery('');
+      return toast.error('Trying to find some news? Enter the search query first!');
+    }
+    //toast---------------------------------------------------
     const filteredNews = news.filter(el => el.description.includes(query) || el.title.includes(query))
   console.log(filteredNews);
   if(filteredNews.length === 0){setNotFound(true)}
@@ -27,7 +33,7 @@ const NewsPage = () => {
 }, []);
 
   return(
-    <>
+    <main>
     <Section title={'News'}>
         {/* <Searchbar ></Searchbar> */}
         <NewsSearch setQuery={setQuery} handleSubmit={handleSubmit} query={query}></NewsSearch>
@@ -36,8 +42,8 @@ const NewsPage = () => {
     
     }
     {!notFound  ? <NewsList data={news} /> : <NotFoundPage />}
-    
-    </>
+    <ToastContainer />
+    </main>
   );
 };
 
