f0e8215bf550f9e03bcb717231baed156ea1bf61
Krystyna Sharhorodska <rinjara@gmail.com>
2023-02-16 18:35:13 +0200

    Did Empty placeholder

--- src/components/PetsData/PetList/PetsList.styled.jsx
+++ src/components/PetsData/PetList/PetsList.styled.jsx
@@ -40,3 +40,56 @@ export const Card = styled.li`
     padding: 20px;
   }
 `;
+
+export const EmptyImg = styled.img`
+  display: block;
+  /* -webkit-user-select: none; */
+  margin: auto;
+  cursor: zoom-in;
+  background-color: hsl(0, 0%, 80%);
+  /* border-radius: ${p => p.theme.radii.round}; */
+`;
+
+export const ImgWrap = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  border-radius: ${p => p.theme.radii.normal};
+  background-color: ${p => p.theme.colors.white};
+  /* width: 300px; */
+  min-height: 400px;
+  height: 100%;
+  box-shadow: 7px 4px 14px rgba(0, 0, 0, 0.11);
+`;
+
+export const EmptyTitle = styled.h3`
+  text-align: center;
+  font-family: ${p => p.theme.fonts.body};
+  font-weight: ${p => p.theme.fontWeights.medium};
+  font-size: ${p => p.theme.fontSizes[3]}px;
+  color: ${p => p.theme.colors.black};
+  line-height: 1.35;
+  letter-spacing: 0.04em;
+
+  @media ${p => p.theme.device.tablet} {
+    font-size: ${p => p.theme.fontSizes[5]}px;
+  }
+`;
+
+export const Text = styled.p`
+  margin-bottom: 10px;
+  text-align: center;
+  font-family: ${p => p.theme.fonts.body};
+  font-weight: ${p => p.theme.fontWeights.medium};
+  font-size: ${p => p.theme.fontSizes[1]}px;
+  color: ${p => p.theme.colors.gray};
+  line-height: 1.35;
+  letter-spacing: 0.04em;
+
+  @media ${p => p.theme.device.tablet} {
+    font-size: ${p => p.theme.fontSizes[2]}px;
+  }
+`;
