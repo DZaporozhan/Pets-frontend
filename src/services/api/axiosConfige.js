c43aa1928e1ff91c95a0f033836f5f49830ec73e
Denis Zaporozhan <98217616+DZaporozhan@users.noreply.github.com>
2023-02-21 19:18:59 +0200

    Update axiosConfige.js

--- src/services/api/axiosConfige.js
+++ src/services/api/axiosConfige.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'https://pets-api-team1.onrender.com/api/',
-  baseURL: 'http://localhost:65000/api/',
+  baseURL: 'https://pets-api-team1.onrender.com/api/',
 });
 
 export default instance;
