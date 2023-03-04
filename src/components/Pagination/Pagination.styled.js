ae38675a29e355ef09ef500ca0b3dcb98ac8ee85
aleksandr105 <aleksandrsherbina105@gmail.com>
2023-03-04 16:09:21 +0100

    edited effect hover color

--- src/components/Pagination/Pagination.styled.js
+++ src/components/Pagination/Pagination.styled.js
@@ -6,4 +6,18 @@ export const PaginationStyled = styled(Pagination)`
   align-items: center;
   justify-content: center;
   margin-top: 90px;
+  .Mui-selected {
+    background-color: #f59256 !important;
+    color: #ffffff !important;
+  }
+
+  .MuiPaginationItem-root {
+    border: 2px solid #f59256;
+    color: #f59256;
+    &:hover {
+      background-color: #f59256;
+      color: #ffffff;
+      cursor: pointer;
+    }
+  }
 `;
